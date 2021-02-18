330579b402894e872f0b031a36faab9095c5289c
Мартин <martin.samardziev@students.finki.ukim.mk>
2021-02-18 20:13:33 +0100

    Added BlogCategory (actions and views)

---
+++ tockoteka/Controllers/BlogCategoryController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tockoteka.Data;
+using tockoteka.Models;
+
+namespace tockoteka.Controllers
+{
+    public class BlogCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BlogCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // get: /BlogCategory/
+        public IActionResult Index()
+        {
+            IEnumerable<BlogCategory> objList = _db.BlogCategory;
+            return View(objList);
+        }
+        // get: Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // post: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BlogCategory obj)
+        {
+            _db.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        // get: Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.BlogCategory.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // post: Edit
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public IActionResult Edit(BlogCategory obj)
+        {
+            if(ModelState.IsValid)
+            {
+                _db.BlogCategory.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(obj);
+        }
+
+        // get: delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.BlogCategory.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // post: delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.BlogCategory.Find(id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.BlogCategory.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ tockoteka/Models/BlogCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tockoteka.Models
+{
+    public class BlogCategory
+    {
+        public int Id { get; set; }
+        [Display(Name = "Име на категорија")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
